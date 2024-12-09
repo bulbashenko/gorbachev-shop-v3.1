7e00f6ecf038d7624874b651e92f6caadd30b46d
bulbashenko <bulbashenko69@gmail.com>
2024-12-09 19:44:06 +0100

    Remove backend and all backend functionality from frontend || Global update

--- backend/apps/notifications/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('verify-email/<str:token>/', views.verify_email, name='verify-email'),
-    path('password-reset/<str:token>/', views.reset_password, name='password-reset'),
-]
